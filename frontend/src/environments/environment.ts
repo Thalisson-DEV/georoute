015fcdd22548d600da68e09ce46b240a40414c89
Thali <thalissondamiao1@gmail.com>
2026-01-28 17:50:42 -0300

    feat(favicon): favicon modificado

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sua-api-no-railway.up.railway.app/api/v1'
+  apiUrl: 'https://backend-production-929f8.up.railway.app/api/v1'
 };
